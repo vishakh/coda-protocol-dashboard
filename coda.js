7751d3eec0402dcc15af790a8fd47aa8f100dfea
Vishakh <vishakh@users.noreply.github.com>
2019-11-02 02:16:09 -0700

    Initial commit

---
+++ coda.js
@@ -0,0 +1,80 @@
+/*
+This is an example snippet - you should consider tailoring it
+to your service.
+*/
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  const result = await fetch(
+    "http://graphql.o1test.net/graphql",
+    {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName
+      })
+    }
+  );
+
+  return await result.json();
+}
+
+const operationsDoc = `
+  query MyQuery {
+    version
+    blocks(last: 100) {
+      nodes {
+        transactions {
+          userCommands {
+            amount
+            fee
+            to
+            from
+            isDelegation
+            memo
+          }
+        }
+      }
+    }
+  }
+`;
+
+function fetchMyQuery() {
+  return fetchGraphQL(
+    operationsDoc,
+    "MyQuery",
+    {}
+  );
+}
+
+async function startFetchMyQuery() {
+  const { errors, data } = await fetchMyQuery();
+
+  if (errors) {
+    // handle those errors like a pro
+    console.error(errors);
+  }
+
+  // do something great with this precious data
+  console.log(data);
+}
+
+async function runQuery() {
+  const { errors, data } = await fetchMyQuery();
+
+  if (errors) {
+    // handle those errors like a pro
+    console.error(errors);
+  }
+
+  // do something great with this precious data
+  console.log(data);
+
+  return data
+}
+
+//startFetchMyQuery();
